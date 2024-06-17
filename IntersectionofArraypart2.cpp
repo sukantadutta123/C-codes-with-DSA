5467f050aa17b3e5a927176eaa289b25dae7294c
sukantadutta123 <141480350+sukantadutta123@users.noreply.github.com>
2024-06-17 23:37:49 +0530

    Add files via upload

---
+++ IntersectionofArraypart2.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector <int>  IntersectionOfArray(vector <int>a1,int n1, vector <int>a2,int n2){
+    vector <int> interArr;
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n2;j++){
+            if(a1[i]==a2[j]){
+                interArr.push_back(a1[i]);
+            }
+            
+        }
+    }
+    return interArr;
+}
+int main() {
+    int n1, n2;
+
+    cout << "Enter Array -> 1 Size: " << endl;
+    cin >> n1;
+    vector<int> a1(n1);  // Initialize after reading n1
+    cout << "Enter Array 1 Elements: " << endl;
+    for (int i = 0; i < n1; i++) {
+        cin >> a1[i];
+    }
+
+    cout << "Enter Array -> 2 Size: " << endl;
+    cin >> n2;
+    vector<int> a2(n2);  // Initialize after reading n2
+    cout << "Enter Array 2 Elements: " << endl;
+    for (int i = 0; i < n2; i++) {
+        cin >> a2[i];
+    }
+     vector<int> interArr = IntersectionOfArray(a1, n1, a2, n2);
+
+    cout << "Intersection of Arrays: " << endl;
+    for (int i = 0; i < interArr.size(); i++) {
+        cout << interArr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+
+
+}
