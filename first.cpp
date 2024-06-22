7e0335e05cb4a8d36f25f78c0161a1c2dcd228ac
sukantadutta123 <141480350+sukantadutta123@users.noreply.github.com>
2024-06-22 23:53:05 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n,i=1,sum=0;
+     
+    cout<<"Enter a number:- "<<endl;
+    cin>>n;
+    while(i<=n){
+        i++;
+        sum=sum+i;
+        }
+        cout<<sum<<""<<endl;;
+    }
