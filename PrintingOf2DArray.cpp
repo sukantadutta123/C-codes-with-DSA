bda849b387e1ca31e37b028620f3e7b1536d30fa
sukantadutta123 <141480350+sukantadutta123@users.noreply.github.com>
2024-06-23 23:59:23 +0530

    Add files via upload

---
+++ PrintingOf2DArray.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int row, col;
+    int sum=0;
+
+    cout << "Enter Row Size: -" << endl;
+    cin >> row;
+    cout << "Enter Column Size: -" << endl;
+    cin >> col;
+
+    // Correctly initialize the 2D vector after getting row and col sizes
+    vector<vector<int>> a(row, vector<int>(col));
+
+    cout << "Enter Matrix Elements: -" << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    // Optional: print the matrix to verify input
+    cout << "The entered matrix is:" << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << a[i][j] << " ";
+            sum = sum+a[i][j];
+        }
+        cout << endl;
+    }
+    cout<<"Sum is :"<<" "<<sum<<endl;
+
+    return 0;
+}
